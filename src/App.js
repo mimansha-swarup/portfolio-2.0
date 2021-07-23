66617fcae131ae8e7f221b086591202901423881
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-07-23 21:19:21 +0530

    not routed yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 // import {BrowserRouter as Router , Route, Switch} from "react-router-dom"
+import ConnectinDetails from './Components/connectionDetails';
+import FloatEmail from './Components/floatEmail';
 import Navbar from './Components/Navbar';
+import BlogPg from './Pages/Blog';
 import Home from './Pages/Home';
 import ProjectPg from './Pages/Project';
+// FiLinkedin, FiTwitter,FiGithub
 
 
 function App() {
   return (
+    // <Router>
+
+    // <div className="App">
+    //   <Navbar/>
+    //   <Switch>
+    //     <Route to="/projects" component={ProjectPg} />
+    //     <Route to="/blogs" component={BlogPg} />
+    //     <Route exact to="/" component={Home} />
+     
+    //   </Switch>
+      
+    // </div>
+    // </Router>
     <div className="App">
       <Navbar/>
-      <Home />
-      <ProjectPg />
+      <Home/>
+      <ConnectinDetails/>
+      <FloatEmail/>
+      <ProjectPg/>
+      <BlogPg/>
+     
+        {/* <Route to="/projects" component={} />
+        <Route to="/blogs" component={BlogPg} />
+        <Route exact to="/" component={Home} /> */}
+     
+      
       
     </div>
   );
