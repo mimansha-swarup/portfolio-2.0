1c89af883e804d53fa26768d022e4d844ee516f1
mimanhsa-swarup <mimanshaswarup@gmail.com>
2021-07-23 21:18:04 +0530

    update db

---
+++ src/db/BlogLists.js
@@ -0,0 +1,15 @@
+export const  blogdb=[   
+
+    {
+        id:"02",
+        title:"Learn Value, Types and Operators in JavaScript",
+        desc:"Let's see what is data and how machine interpret data.We store our data on Electronic devices for example we stored a image on a computer now this image is been stored in a long sequence of bits",
+        live:"https://dev.to/mimanshaswarup/value-types-and-operators-in-javascript-b39",
+    },
+    {
+        id:"01",
+        title:"Making Node CLI(Command Line Interface) quiz app",
+        desc:"We will use two node modules readline-sync: It can take input in various manner from user chalk: it is an expressive api which adds colors and styling",
+        live:"https://dev.to/mimanshaswarup/making-first-node-cli-command-line-interface-quiz-app-25h",
+    },
+]
---
+++ src/db/ProjectsList.js
@@ -0,0 +1,72 @@
+export const  projdb=[
+   
+     
+    
+    
+    
+    
+    {
+        id:"06",
+        title:"!IMDb",
+        desc:"Movie listing App have diffrent genres, React concept like useState is used.",
+        stack:["Node JS"],
+        code:"https://github.com/mimansha-swarup/dummy-movie-listing-React",
+        live:"https://zdvou.csb.app/",
+        img:"https://raw.githubusercontent.com/mimansha-swarup/dummy-movie-listing-React/main/image.png"
+    },
+    // {
+    //     id:"05",
+    //     title:"Spotify UI Clone",
+    //     desc:"UI Clone of Spotify,React Concepts like useEffect,useState,React ROuting is Used",
+    //     stack:["React JS"],
+    //     code:"https://github.com/mimansha-swarup/spotify-ui-clone",
+    //     live:"https://myspotify-ui-clone.netlify.app/",
+    //     img:"https://raw.githubusercontent.com/mimansha-swarup/emoji-dictionary-react/main/weathomoji.png"
+    // }, 
+    {
+        id:"05",
+        title:"Weathomoji",
+        desc:"it's a React app which let you know the meaning of weather emojis concept like useState is used.",
+        stack:["React JS"],
+        code:"https://github.com/mimansha-swarup/spotify-ui-clone",
+        live:"https://myspotify-ui-clone.netlify.app/",
+        img:"https://raw.githubusercontent.com/mimansha-swarup/emoji-dictionary-react/main/weathomoji.png"
+    }, 
+    {
+        id:"04",
+        title:"What Will Groot say...",
+        desc:"it's a funny translation web app an translation api is used and basic Dom concept",
+        stack:["HTML","CSS","JavaScript"],
+        code:"https://github.com/mimansha-swarup/groot-translate",
+        live:"https://i-am-grooot.netlify.app/",
+        img:"https://raw.githubusercontent.com/mimansha-swarup/groot-translate/main/img/groot-web.png"
+    },
+    {
+        id:"03",
+        title:"Minion Says...",
+        desc:"it's a very basic web app an translation api is used and basic Dom concept",
+        stack:["HTML","CSS","JavaScript"],
+        code:"https://github.com/mimansha-swarup/Learning-how-to-use-API",
+        live:"https://minion-translation.netlify.app/",
+        img:"https://raw.githubusercontent.com/mimansha-swarup/Learning-how-to-use-API/main/output/desktopview.png"
+    }, 
+    {
+        id:"02",
+        title:"Marvel CLI Quiz",
+        desc:"It's a normal quiz on marvel one can play on command line interface. Various packages of Node JS is used like chalk and readline sync",
+        stack:["Node JS"],
+        code:"https://github.com/mimansha-swarup/basic-quiz-cli-app-js",
+        live:"https://replit.com/@MimanshaSwarup/neog-assignment1",
+        img:"https://raw.githubusercontent.com/mimansha-swarup/basic-quiz-cli-app-js/main/image.png"
+    }, 
+    {
+         id:"01",
+         title:"Basic CLI App",
+         desc:"Various packages of Node JS is used like chalk and readline sync",
+         stack:["Node JS"],
+         code:"https://github.com/mimansha-swarup/assignment-1-neog-camp",
+         live:"https://replit.com/@MimanshaSwarup/cli-app#index.js",
+         img:"https://raw.githubusercontent.com/mimansha-swarup/assignment-1-neog-camp/main/image.png"
+     },
+    
+]
